a306abaaeb216a1b214eadb5dcf7f9f3d4bec640
Abeltin <45063096+Abeltin@users.noreply.github.com>
2024-11-10 22:17:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-class Men:
+class Man:
     """Базовый класс для людей"""
     def __init__(self, name):
         self.name = name
 
-class Student(Men):
+class Student(Man):
     """Класс студентов"""
     def __init__(self, name, group):
         super().__init__(name)
         self.group = group
 
-class Teacher(Men):
+class Teacher(Man):
     """Класс преподавателей"""
     def __init__(self, name, subject):
         super().__init__(name)
         self.subject = subject
 
-class MenFactory:
+class ManFactory:
     """
     Фабричный класс, который создает объекты студентов или преподавателей
     """
     @staticmethod
-    def create_men(role, name, extra_info):
+    def create_man(role, name, extra_info):
         if role == "Студент":
             return Student(name, extra_info)
         elif role == "Преподаватель":
@@ -28,8 +28,8 @@ class MenFactory:
         else:
             raise ValueError(f"Роль {role} не поддерживается.")
           
-student = MenFactory.create_men("Студент", "Семен Абельтин", "Группа 41ИС-21")
-teacher = MenFactory.create_men("Преподаватель", "Леонид Борисович", "ОООП")
+student = ManFactory.create_man("Студент", "Семен Абельтин", "Группа 41ИС-21")
+teacher = ManFactory.create_man("Преподаватель", "Леонид Борисович", "ОООП")
 
 print(f"{student.name} из {student.group}")
 print(f"{teacher.name} преподает {teacher.subject}") 
